b3bb82d8cd64ceaadc0a7d745069a7f1154cccf0
Ron Kurr <kurr@kurron.org>
2017-12-05 21:34:51 -0500

    Lab 21: Docker Machine

--- solutions/lab-21/create-alpha-machine.sh
+++ solutions/lab-21/create-alpha-machine.sh
@@ -2,28 +2,24 @@
 
 #docker-machine create --driver amazonec2 --amazonec2-access-key AKI******* --amazonec2-secret-key 8T93C*******  aws-sandbox
 
-AMI=$(curl --silent http://169.254.169.254/latest/meta-data/ami-id)
 INSTANCE_TYPE=$(curl --silent http://169.254.169.254/latest/meta-data/instance-type)
 INTERFACE=$(curl --silent http://169.254.169.254/latest/meta-data/network/interfaces/macs/)
 SUBNET_ID=$(curl --silent http://169.254.169.254/latest/meta-data/network/interfaces/macs/${INTERFACE}/subnet-id)
 VPC_ID=$(curl --silent http://169.254.169.254/latest/meta-data/network/interfaces/macs/${INTERFACE}/vpc-id)
-
+NAME="alpha"
 # These need to match your account
 REGION="us-west-2"
-SECURITY_GROUP_ID="sg-ef8dc992"
 
+# Can anyone spot missing information usually required to create an EC2 instance?
 CMD="docker-machine create --driver amazonec2 \
-                           --amazonec2-ami ${AMI} \
                            --amazonec2-instance-type ${INSTANCE_TYPE} \
                            --amazonec2-region ${REGION} \
                            --amazonec2-ssh-keypath ./study-group \
-                           --amazonec2-ssh-user ubuntu \
-                           --amazonec2-tags Name,alpha,project,study-group \
+                           --amazonec2-tags Name,${NAME},project,study-group \
                            --amazonec2-subnet-id ${SUBNET_ID} \
                            --amazonec2-vpc-id ${VPC_ID} \
-                           --amazonec2-security-group ${SECURITY_GROUP_ID} \
                            --engine-label size=small \
                            --engine-label role=console \
-                           alpha"
+                           ${NAME}"
 echo ${CMD}
 ${CMD}
