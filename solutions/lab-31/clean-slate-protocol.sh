be76dbe136917ef2f1135243086ea491c22862f9
Ron Kurr <kurr@kurron.org>
2018-01-14 20:59:17 +0000

    steps to recreate the cluster

--- solutions/lab-31/clean-slate-protocol.sh
+++ solutions/lab-31/clean-slate-protocol.sh
@@ -4,6 +4,10 @@ docker stop $(docker ps --quiet)
 docker rm --volumes --force $(docker ps --all --quiet)
 docker rmi --force $(docker images --quiet)
 
+# docker-machine stop alpha bravo charlie delta echo
+# docker-machine start alpha bravo charlie delta echo
+# docker-machine regenerate-certs alpha bravo charlie delta echo
+
 docker-machine ssh bravo docker service rm hello-global
 docker-machine ssh bravo docker service rm hello-constrained
 docker-machine ssh bravo docker service rm hello-replicated
