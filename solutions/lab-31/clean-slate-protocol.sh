f48a559d38b545013d06edcdb086b17f9d7a7d65
Ron Kurr <kurr@kurron.org>
2018-01-14 20:03:31 +0000

    Remove all services from the cluster

--- solutions/lab-31/clean-slate-protocol.sh
+++ solutions/lab-31/clean-slate-protocol.sh
@@ -3,3 +3,10 @@
 docker stop $(docker ps --quiet)
 docker rm --volumes --force $(docker ps --all --quiet)
 docker rmi --force $(docker images --quiet)
+
+docker-machine ssh bravo docker service rm hello-global
+docker-machine ssh bravo docker service rm hello-constrained
+docker-machine ssh bravo docker service rm hello-replicated
+docker-machine ssh bravo docker service rm nginx
+docker-machine ssh bravo docker service rm redis
+docker-machine ssh bravo docker service ls
